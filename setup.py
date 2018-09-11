14482745bf50aa7280de3aa9565a41465a3d52c7
Jim Laney <jlaney@uw.edu>
2018-09-11 10:57:44 -0700

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='tech-inventory-update',
+    version='0.1',
+    packages=[],
+    author="UW-IT AXDD",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=[
+        'google-auth',
+        'requests',
+        'gspread',
+        'PyYAML',
+    ],
+    license='Apache License, Version 2.0',
+    description=('AXDD Technology spreadsheet updater'),
+    url='https://github.com/uw-it-aca/tech-inventory-update',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
